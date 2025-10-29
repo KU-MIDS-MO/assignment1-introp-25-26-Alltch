4b118f98a8cc63d04103aa31b222020e3e130d68
Alltch <alltchumb05@gmail.com>
2025-10-29 20:56:34 +0100

    done

--- sum_of_cubes_even.py
+++ sum_of_cubes_even.py
@@ -2,13 +2,11 @@ def sum_of_cubes_even(n):
     if not isinstance(n, int) or n < 0:
         return -1
     if n > 2000:
-        return "warning but still compute"
-    sum = 0
+        print("warning: but still compute")
+    total = 0
     for i in range(0, n + 1):
         if i % 2 == 0:
-            sum += i * i * i
-        else:
-            continue
-    return float(sum)
+            total += i ** 3
+    return float(total)
 
 print(sum_of_cubes_even(6))
